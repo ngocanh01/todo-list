4772ec440eeffaba8e9921b490d3380dcc97c0de
ngocanh01 <nanh5605@gmail.com>
2025-03-31 10:25:04 +0700

    init

---
+++ todo-list/src/components/Contact/index.jsx
@@ -0,0 +1,12 @@
+import "./Contact.styles.css";
+const Contact = () => {
+  return (
+    <div className="about-container">
+      <h1 className="about-heading">
+        Contact Us
+      </h1>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ todo-list/src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.styles.css'
+
+const Footer = ()=>{
+    return(
+        <div className="footer-container">
+            {/* <div className="footer-heading"> */}
+            &copy; 2025
+            {/* </div> */}
+        </div>
+    )
+}
+
+
+export default Footer;
